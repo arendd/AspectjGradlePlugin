8e5c50451ad7c5259af259ea61bea5e5b3e3fb87
Benedikt Ritter <beneritter@gmail.com>
2020-09-16 17:30:02 +0200

    Read aspectjVersion lazily
    Instead of eagerly evaluating the aspectjVersion project extension we
    now access it lazily using Gradle's provider API. This makes it possible
    to apply the plugin via the plugins block but results in a default error
    message being displayed in case the aspectjVersion is not present.
    
    Resolves #2

---
+++ src/test/groovy/aspectj/AspectJGradlePluginFuncTest.groovy
@@ -0,0 +1,63 @@
+package aspectj
+
+import org.gradle.testkit.runner.BuildResult
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+import spock.lang.Specification
+
+import java.lang.management.ManagementFactory
+
+class AspectJGradlePluginFuncTest extends Specification {
+
+    @Rule
+    TemporaryFolder projectDir
+
+    def buildFile
+
+    void setup() {
+        buildFile = projectDir.newFile("build.gradle")
+    }
+
+    def "can be applied via plugins block"() {
+        given:
+        buildFile << """
+        plugins {
+            id 'aspectj.AspectjGradlePlugin'
+        }
+        project.ext.aspectjVersion = '1.9.6'
+        """
+
+        expect:
+        success("build")
+    }
+
+    def "fails the build if aspectjVersion is not set as project extension"() {
+        given:
+        buildFile << """
+        plugins {
+            id 'aspectj.AspectjGradlePlugin'
+        }
+        """
+
+        expect:
+        fail("build")
+    }
+
+    def success(String... args) {
+        runner(args).build()
+    }
+
+    def fail(String... args) {
+        runner(args).buildAndFail()
+    }
+
+    private runner(String... args) {
+        GradleRunner.create()
+                .forwardOutput()
+                .withDebug(ManagementFactory.getRuntimeMXBean().getInputArguments().toString().indexOf("-agentlib:jdwp") > 0)
+                .withPluginClasspath()
+                .withArguments([*args, "-s"])
+                .withProjectDir(projectDir.root)
+    }
+}
